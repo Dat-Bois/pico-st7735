678e94980c3f9e255021a557d92a51bdb5b7f288
unknown <eeshvij@gmail.com>
2023-03-12 10:53:23 -0700

    testing ext functions

---
+++ functions-st7735.h
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdint.h>
+#include "adafruit-st7735.h"
+#include <adafruit-gfx.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    adafruit_gfx *gfx;
+    void *obj;
+} st7735;
+
+void testlines(st7735 *st, uint16_t color);
+void testdrawtext(st7735 *st, char *text, uint16_t color);
+
+#ifdef __cplusplus
+}
+#endif
